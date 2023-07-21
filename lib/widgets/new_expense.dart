930d8637f777998bd9edc3a5bec28ea31f723cd6
Clebson Moura <clebson.sh@gmail.com>
2023-07-21 12:12:17 -0300

    Handling User (Text) Input with the TextField Widget

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpense();
+  }
+}
+
+class _NewExpense extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            maxLength: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
